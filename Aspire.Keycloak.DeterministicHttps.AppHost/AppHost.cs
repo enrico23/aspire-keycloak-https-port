5b191d8e6ce4e57af5380191573f0e733761279d
enrico23 <enrico2371@gmail.com>
2026-01-08 18:31:50 +0000

    Fix

--- Aspire.Keycloak.DeterministicHttps.AppHost/AppHost.cs
+++ Aspire.Keycloak.DeterministicHttps.AppHost/AppHost.cs
@@ -3,10 +3,4 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder.AddProject<Projects.Aspire_Keycloak_DeterministicHttps_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
-builder.AddProject<Projects.Aspire_Keycloak_DeterministicHttps_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
 builder.Build().Run();
